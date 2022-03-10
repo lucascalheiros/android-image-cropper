7c92e36d1eb743e012ba3354bbc4a71e7893ed83
Lucas Calheiros <lucascalheirosjllc@gmail.com>
2022-03-10 00:04:12 -0300

    add planned structures

---
+++ app/src/main/java/com/example/imagecropper/ImageCropperActivity.kt
@@ -0,0 +1,11 @@
+package com.example.imagecropper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageCropperActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_cropper)
+    }
+}
